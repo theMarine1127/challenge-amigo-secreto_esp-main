5685e84e2226af98a33e2428448ce39e57ba8df3
Jhon D. Marin <j.hondmarin@hotmail.com>
2025-01-26 10:41:51 -0500

    Start Proyect Amigo seccreto

---
+++ app.js
@@ -0,0 +1,6 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+
+function agregarAmigo() {
+
+}
