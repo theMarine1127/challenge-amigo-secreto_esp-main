476e4df0a9a13c3d0ee432f8db94c35cec750ffc
Jhon D. Marin <j.hondmarin@hotmail.com>
2025-01-30 22:27:10 -0500

    function agregarAmigo, se inserto preguntareinicio() y una validacion para que no se repitiera nombre,

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 const amigos = [];
+let respuesta = true;
 
 function asignarTextElement(elemento, texto) { //function para asiganar text
     let elementHtml = document.querySelector(elemento);
@@ -9,15 +10,24 @@ function asignarTextElement(elemento, texto) { //function para asiganar text
 }
 
 function agregarAmigo() {
-    let addAmigo = document.getElementById("amigo").value;
-    if (addAmigo.trim() === "") { // Verifica si el valor está vacío o contiene solo espacios
-        alert("Debe ingresar un nombre.")
-    } else {
-        amigos.push(addAmigo);
-        alert(`Amigo agregado: ${addAmigo}`);
-        console.log(amigos);
+    let addAmigo = document.getElementById("amigo").value.trim();
+    if (addAmigo === "") { // Verifica si el valor está vacío
         limpiaNombre();
-        mostrarAmiguis();
+        amigos.length = 0;
+        preguntaReinicio();
+        asignarTextElement('#listaAmigos', ` `);
+
+        return;
+    } else {
+        if (amigos.includes(addAmigo)) {
+            alert(`El nombre ${addAmigo} ya se encuentra en la lista`);
+            limpiaNombre();
+        } else {
+            amigos.push(addAmigo);
+            alert(`Amigo agregado: ${addAmigo}`);
+            mostrarAmiguis();
+            limpiaNombre();
+        }
     }
     return;
 }
@@ -38,15 +48,30 @@ function limpiaNombre() {
     document.getElementById("amigo").value = "";
 }
 
+
+
+
+
 function sortearAmigo() {
-    if (amigos.length > 0) {
-        const amigoIndex = Math.floor(Math.random() * amigos.length);
-        const aleatorioAmigo = amigos[amigoIndex];
-        asignarTextElement('ul', `El amigo seleccionado es: ${aleatorioAmigo}`);
-        //return aleatorioAmigo;
-    } else {
+    if (amigos.length === 0) {  // Primero validamos si hay amigos en la lista
         alert("Debe registrar amigos.");
         return null;
+    } else {
+        const amigoIndex = Math.floor(Math.random() * amigos.length);
+        const aleatorioAmigo = amigos[amigoIndex];
+        asignarTextElement('#listaAmigos', `El amigo seleccionado es: ${aleatorioAmigo} `);
+
     }
+
 }
 
+function preguntaReinicio() {
+    const respuesta = confirm("Quiere volver a jugar?");
+    if (respuesta) {
+        location.reload(); //esto regarga la pagina
+    } else {
+        alert("Sorteo finalizado.");
+        amigos.length = 0;
+        document.querySelector("#listaAmigos").innerHTML = "";
+    }
+}
